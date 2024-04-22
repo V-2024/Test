d667a703c8204226c44064d651d8f1d6e008dd24
ygurwls <alvin20@naver.com>
2024-04-22 18:01:17 +0900

    start

---
+++ Source/RuinsHunter/RuinsHunterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RuinsHunterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUINSHUNTER_API ARuinsHunterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
